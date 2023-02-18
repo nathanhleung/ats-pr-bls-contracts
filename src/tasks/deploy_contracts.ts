87610f00c1891d3583a22df760670a81d93a2d77
nathanhleung <github@nathanhleung.com>
2023-02-18 13:58:35 -0800

    fix: disable etherscan verification

--- src/tasks/deploy_contracts.ts
+++ src/tasks/deploy_contracts.ts
@@ -6,7 +6,8 @@ task("deploy-contracts", "Deploys and verifies Safe contracts")
     .setAction(async (_, hre) => {
         await hre.run("deploy")
         await hre.run("local-verify")
-        await hre.run("etherscan-verify", { forceLicense: true, license: 'LGPL-3.0'})
+        // Disable Etherscan verification
+        // await hre.run("etherscan-verify", { forceLicense: true, license: 'LGPL-3.0'})
     });
 
 export { }
